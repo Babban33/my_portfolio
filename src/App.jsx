13bf9c8fd4a9b131db819f2b281243db2cc7b826
Babban33 <chaitanyawankhede705@gmail.com>
2024-08-18 13:54:43 +0530

    added bg and content area

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { useEffect } from 'react';
 import './App.css'
+import Preloader from './preloader/preload';
 
-function App() {
-
-  return (
-    <>
-      Hello
-    </>
-  )
+function App (){
+    return(
+        <div className='bg-back'>
+            <Preloader></Preloader>
+            <div className='page block w-full h-full min-h-full overflow-hidden py-0 px-[100px]'>
+                <div className='page-content relative w-full max-w-[1280px] min-h-[80vh] my-[10vh] p-0 bg-[#444] shadow-[0_0_100px_-5px_rgba(0,0,0,.25)] rounded-[32px] backface-hidden'>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
