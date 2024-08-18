0c3201d3608a519fb7baff0c7d3e54be51627e05
Babban33 <chaitanyawankhede705@gmail.com>
2024-08-18 18:59:53 +0530

    removed inline css

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ function App (){
     return(
         <div className='bg-back main'>
             <Preloader></Preloader>
-            <div className='page block w-full h-full min-h-full overflow-hidden py-0 px-[100px]'>
-                <div className='page-content relative w-full max-w-[1280px] min-h-[80vh] my-[10vh] p-0 bg-[#444] shadow-[0_0_100px_-5px_rgba(0,0,0,.25)] rounded-[32px] backface-hidden'>
+            <div className='page'>
+                <div className='page-content'>
                     <Header></Header>
+                    <div className="menu-toggle">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
             </div>
         </div>
