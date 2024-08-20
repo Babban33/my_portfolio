73cb3e2b2a0dba82c7adbf0520f044d466479f98
Babban33 <chaitanyawankhede705@gmail.com>
2024-08-20 17:46:30 +0530

    added navArrows

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import './App.css'
 import Preloader from './preloader/preload';
 import Header from './landing/header';
-
+import NavArrows from './landing/navArrows';
 function App (){
     return(
         <div className='bg-back main'>
@@ -10,6 +10,12 @@ function App (){
             <div className='page block w-full h-full min-h-full overflow-hidden py-0 px-[100px]'>
                 <div className='page-content relative w-full max-w-[1280px] min-h-[80vh] my-[10vh] p-0 bg-[#444] shadow-[0_0_100px_-5px_rgba(0,0,0,.25)] rounded-[32px] backface-hidden'>
                     <Header></Header>
+                    <div className="menu-toggle">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <NavArrows></NavArrows>
                 </div>
             </div>
         </div>
---
+++ src/landing/navArrows.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../App.css";
+import "../responsive.css";
+function NavArrows(){
+    return(
+        <div className="lmpixels-arrows-nav">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default NavArrows;
